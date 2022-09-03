5f11f614f2f38a3a78e1113195e14864ea84b8e0
mohsin19295 <mohsin19295@gmail.com>
2022-09-03 12:15:33 +0530

    segement of ones added

---
+++ Easy/segmentOfOnes.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+
+var checkOnesSegment = function (s) {
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] == "1" && s[i - 1] == "0") return false;
+    }
+    return true;
+};
+
+console.log(checkOnesSegment('1001'))
+console.log(checkOnesSegment('110'))
