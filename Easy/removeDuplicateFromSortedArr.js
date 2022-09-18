b3825c7a5bbc4664ae91b63f676880c22b2a4d4b
mohsin19295 <mohsin19295@gmail.com>
2022-09-18 18:54:40 +0530

    removeDuplicateFromSortedArr added

---
+++ Easy/removeDuplicateFromSortedArr.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+var removeDuplicates = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
+console.log( removeDuplicates([1,1,2,2,5]))
+console.log( removeDuplicates([1,1,2]))
