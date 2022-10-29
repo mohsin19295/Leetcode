64bce32ef4881d6699a8991aa906537acd5ec197
mohsin19295 <mohsin19295@gmail.com>
2022-10-29 20:09:25 +0530

    climbingStairs added

---
+++ Easy/climbingStairs.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+var climbStairs = function (n) {
+  let dp = new Array(n).fill(0);
+  dp[0] = 1;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+
+
+console.log(climbStairs(2))
+console.log(climbStairs(3))
+console.log(climbStairs(4))
+console.log(climbStairs(5))
