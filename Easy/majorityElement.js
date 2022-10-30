965194215a625655277c2307c7f506125b71be3c
mohsin19295 <mohsin19295@gmail.com>
2022-10-30 21:56:51 +0530

    majority Element added

---
+++ Easy/majorityElement.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/majority-element/
+
+var majorityElement = function (nums) {
+  let data = {};
+  for (let i = 0; i < nums.length; i++) {
+    let k = nums[i];
+    if (data[k])  data[k] += 1;
+    else data[k] = 1;
+  }
+  let max = 0;
+  for (key in data) {
+    if (data[key] > nums.length / 2) max = data[key];
+    if (max)  return key;
+  }
+};
+
+console.log(majorityElement([3, 2, 3]));
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
